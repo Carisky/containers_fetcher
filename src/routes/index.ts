7addd49cef47047c74cdde91d5c28f2b3a3760a1
Yehor <egorkorsungit@gmail.com>
2025-10-23 11:30:56 +0200

    add google sheets service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import containerRoutes from "./containerRoutes";
 import firebirdRoutes from "./firebirdRoutes";
+import googleRoutes from "./googleRoutes";
 import utilsRoutes from "./utilsRoutes";
 
 const routes = Router();
 
 routes.use("/", containerRoutes);
 routes.use("/huzar/winsad/db", firebirdRoutes);
+routes.use("/google", googleRoutes);
 routes.use("/utils", utilsRoutes);
 
 export default routes;
